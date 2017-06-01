027e58faafc42c9fdddfdc93df53503aab78b03e
Siyuan Liu <tomato.sliu@gmail.com>
2017-05-31 19:45:50 -0700

    update eval

--- evaluate/lane.py
+++ evaluate/lane.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression
+import ujson as json
 
 
 class LaneEval(object):
@@ -26,7 +27,7 @@ class LaneEval(object):
 
     @staticmethod
     def bench(pred, gt, y_samples, running_time):
-        if running_time > 200:
+        if running_time > 200 or any(len(p) != len(y_samples) for p in pred):
             return 0., 0., 1.
         angles = [LaneEval.get_angle(np.array(x_gts), np.array(y_samples)) for x_gts in gt]
         threshs = [LaneEval.pixel_thresh / np.cos(angle) for angle in angles]
@@ -35,13 +36,43 @@ class LaneEval(object):
         matched = 0.
         for x_gts, thresh in zip(gt, threshs):
             accs = [LaneEval.line_accuracy(np.array(x_preds), np.array(x_gts), thresh) for x_preds in pred]
-            max_acc = np.max(accs)
+            max_acc = np.max(accs) if len(accs) > 0 else 0.
             if max_acc < LaneEval.pt_thresh:
                 fn += 1
             else:
                 matched += 1
             line_accs.append(max_acc)
         fp = len(pred) - matched
-        if len(gt) > 4:
+        if len(gt) > 4 and fn > 0:
             fn -= 1
-        return sum(line_accs) / min(4.0, len(gt)), fp / len(pred), fn / len(gt)
+        s = sum(line_accs)
+        if len(gt) > 4:
+            s -= min(line_accs)
+        return s / max(min(4.0, len(gt)), 1.), fp / len(pred) if len(pred) > 0 else 0., fn / max(len(gt) , 1.)
+
+    @staticmethod
+    def bench_one_submit(pred_file, gt_file):
+        json_pred = [json.loads(line) for line in open(pred_file).readlines()]
+        json_gt = [json.loads(line) for line in open(gt_file).readlines()]
+        gts = {l['raw_file']: l for l in json_gt}
+        accuracy, fp, fn = 0., 0., 0.
+        for pred in json_pred:
+            raw_file = pred['raw_file']
+            pred_lanes = pred['lanes']
+            #  run_time = pred['run_time']
+            run_time = 0.
+            if raw_file not in gts:
+                fn += 1.
+                continue
+            gt = gts[raw_file]
+            gt_lanes = gt['lanes']
+            y_samples = gt['h_samples']
+            a, p, n = LaneEval.bench(pred_lanes, gt_lanes, y_samples, run_time)
+            accuracy += a
+            fp += p
+            fn += n
+        num = len(gts)
+        return json.dumps({'Accuracy': accuracy / num, 'FP': fp / num, 'FN': fn / num})
+
+print LaneEval.bench_one_submit('/mnt/truenas/scratch/sliu/benchmark/test_data.json',
+                                '/mnt/truenas/scratch/sliu/benchmark/label_data.json')
