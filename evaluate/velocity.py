eb5e8def3307d76b574f88bd8b000103e0ea2a16
chenyanl <lichenyangsun1@gmail.com>
2017-06-05 01:16:04 -0700

    velocity estimation interface

--- evaluate/velocity.py
+++ evaluate/velocity.py
@@ -3,26 +3,42 @@ import json
 
 
 class VeloEval(object):
-
     @staticmethod
-    def load_annotation(file_list):
+    def load_json_file(file_list):
         data_list = []
-        for file_name in file_list:
-            with open(file_name) as data_file:
-                raw_data = json.load(data_file)
-                data = []
-                for instance in raw_data:
-                    instance["bbox"] = np.array([[instance["bbox"]["top"],
-                                                  instance["bbox"]["left"],
-                                                  instance["bbox"]["bottom"],
-                                                  instance["bbox"]["right"]]])
-                    data.append(instance)
-            data_list.append(data)
-        print "Finished loading {0:d} annotations.".format(len(file_list))
+        for file_name in file_list: 
+            with open(file_name) as f:
+                raw_data = json.load(f)
+            data_list.append(raw_data)
         return data_list
 
     @staticmethod
+    def transform_annotation(raw_data_list):
+        anno_list = []
+        for raw_data in raw_data_list:
+            data = []
+            for instance in raw_data:
+                instance["bbox"] = np.array([[instance["bbox"]["top"],
+                                              instance["bbox"]["left"],
+                                              instance["bbox"]["bottom"],
+                                              instance["bbox"]["right"]]])
+                data.append(instance)
+            anno_list.append(data)
+        return anno_list                                  
+
+    @staticmethod
+    def load_annotation(file_list):
+        raw_data_list = VeloEval.load_json_file(file_list)
+        anno_list = VeloEval.transform_annotation(raw_data_list)
+        print "Finished loading {0:d} annotations.".format(len(anno_list))
+        return anno_list
+
+    @staticmethod
     def calc_error(a, b):
+        try:
+            error = np.linalg.norm(np.array(a)-np.array(b)) ** 2
+        except BaseException as e:
+            raise Exception('Error data format')
         return np.linalg.norm(np.array(a)-np.array(b)) ** 2
 
     @staticmethod
@@ -39,6 +55,8 @@ class VeloEval(object):
     def find_nearest_gt(pred, x_gt):
         bboxes = np.vstack([x["bbox"] for x in x_gt])
         difference = np.sum(np.abs(np.subtract(bboxes, pred["bbox"])), axis=1)
+        if np.min(difference) > 5:
+            raise Exception('We do not get all the predictions for a certain frame')
         return x_gt[np.argmin(difference)]
 
     @staticmethod
@@ -67,3 +85,34 @@ class VeloEval(object):
         print "Position Estimation error (Far): {0:.5f}".format(pe2)
         print "Position Estimation error total: {0:.5f}".format((pe0+pe1+pe2)/3)
         return (ve0+ve1+ve2)/3, (pe0+pe1+pe2)/3
+    
+    @staticmethod
+    def bench_one_submit(pred_file, gt_file):
+        try:
+            json_pred = [json.loads(line) for line in open(pred_file).readlines()]
+            json_gt = [json.loads(line) for line in open(gt_file).readlines()]
+        except BaseException as e:
+            raise Exception('Fail to load json file of the prediction.')
+        if len(json_gt) != len(json_pred):
+            raise Exception('We do not get the predictions of all the test tasks')
+        pred_list = VeloEval.transform_annotation(json_pred)
+        gt_list = VeloEval.transform_annotation(json_gt)
+        pos_error = [[], [], []]
+        velo_error = [[], [], []]
+        for x_pred, x_gt in zip(pred_list, gt_list):
+            for gt in x_gt:
+                pred = VeloEval.find_nearest_gt(gt, x_pred)
+                distance_label = VeloEval.get_distance_label(gt)
+                if position not in pred or velocity not in pred:
+                    raise Exception('Missing position or velocity')
+                pos_error[distance_label].append(VeloEval.calc_error(pred["position"], gt["position"]))
+                velo_error[distance_label].append(VeloEval.calc_error(pred["velocity"], gt["velocity"]))
+        
+        ve0 = np.mean(np.array(velo_error[0]))
+        ve1 = np.mean(np.array(velo_error[1]))
+        ve2 = np.mean(np.array(velo_error[2]))
+        pe0 = np.mean(np.array(pos_error[0]))
+        pe1 = np.mean(np.array(pos_error[1]))
+        pe2 = np.mean(np.array(pos_error[2]))
+
+        return (ve0+ve1+ve2)/3, ve0, ve1, ve2, (pe0+pe1+pe2)/3, pe0, pe1, pe2, pe3
